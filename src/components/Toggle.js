49eda91bb400b851b3d75c2a7af6cc0dfe71e73c
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-02-02 09:32:54 -0800

    commit tree 1f9a164f31b1ead4996bff95c4904604db4f160e

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false)
+  function handleClick(){
+    setIsOn((isOn) => !isOn)
+  }
+  const color = isOn ? "red" : "white"
+
+  !isOn ? console.log("Turned me Off") : console.log("Turned me On")
+  
+  return <button style={{ background: color }} onClick={handleClick}>{!isOn ? "Off" : "On"}</button>;
+  
+  
+
 }
 
 export default Toggle;
